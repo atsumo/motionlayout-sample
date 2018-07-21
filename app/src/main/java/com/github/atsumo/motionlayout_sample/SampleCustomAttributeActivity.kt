536679ac85886946e6a3a54509fa7d968b3c050c
atsumo <bachisuta@gmail.com>
2018-07-21 17:12:49 +0900

    keyframesetとcustomattributeのサンプル追加

---
+++ app/src/main/java/com/github/atsumo/motionlayout_sample/SampleCustomAttributeActivity.kt
@@ -0,0 +1,12 @@
+package com.github.atsumo.motionlayout_sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SampleCustomAttributeActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_sample_custom_attribute)
+  }
+}
